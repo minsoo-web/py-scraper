c10735f225a92b4c37b724f4df65bedfb4fca7ad
minsoo kim <zlemzlem5656@naver.com>
2019-12-12 00:25:28 +0900

    Update Sky-line.cpp
    not complete yet

--- Linked-List/Sky-line.cpp
+++ Linked-List/Sky-line.cpp
@@ -2,134 +2,212 @@
 using namespace std;
 
 class Node {
-   friend class skyline;
+	friend class skyline;
 private:
-   Node* next;
-   int location;
-   int height;
+	Node* next;
+	int location;
+	int height;
 public:
-   Node() {
-      next = NULL;
-      location = 0;
-      height = 0;
-   }
-   Node(int _location, int _height) {
-      location = _location;
-      height = _height;
-      next = NULL;
-   }
+	Node() {
+		next = NULL;
+		location = 0;
+		height = 0;
+	}
+	Node(int _location, int _height) {
+		location = _location;
+		height = _height;
+		next = NULL;
+	}
 };
 
 class skyline {
 private:
-   Node* head;
+	Node* head;
 public:
-   skyline()
-   {
-      head = NULL;
-   }
-   skyline(int left, int height, int right)
-   {
-      if (left < right) // 옳은 경우의 생성자
-      {
-         Node* leftbuilding = new Node(left, height);
-         Node* rightbuilding = new Node(right, height);
-         if (head == NULL) { //처음 삽입
-            head = leftbuilding;
-            head->next = rightbuilding;
-         }
-         else { // 하나의 노드라도 존재한다면
-            search(head, leftbuilding);  // 왼쪽 빌딩을 먼저 추가 해준 뒤
-            search(leftbuilding, rightbuilding);  // 오른쪽 빌딩을 추가 해준다.
-         }
-      }
-      else {      // 잘못된 경우의 생성자 --> 오류
-         return;
-      }
-   }
-   Node* search(Node* _currentNode, Node* input_building) {
-      if (_currentNode->location < input_building->location)
-      {
-         if (_currentNode->next->location > input_building->location) { // 현재 노드의 다음 노드의 위치 보다 작다면
-            Node* temp = _currentNode->next;
-            _currentNode->next = input_building;
-            input_building->next = temp;
-         }
-         else {
-            search(_currentNode->next, input_building);
-         }
+	skyline()
+	{
+		head = NULL;
+	}
+	skyline(int _left, int _height, int _right) {
+		if (_left < _right) {
+			head = NULL;
+			Node* left = new Node(_left, _height);
+			Node* right = new Node(_right, _height);
+			head = left;
+			head->next = right;
+			right->next = NULL;
+		}
+		else {
+			cout << "잘못된 경우의 생성입니다" << endl;
+			head = NULL;
+		}
+	}
 
-      }
-      else if (_currentNode->location > input_building->location) {
-         input_building->next = _currentNode;
-      }
-      else { // _currentNode의 위치가 input_building의 위치와 같은 경우 -> height를 비교해주어야 한다
-         if (_currentNode->height!=input_building->height) { // 같지 않다면 비교해야한다
-            if (_currentNode->height > input_building->height) { // 원래 높이가 더 높다는 의미이므로 새로 고침해줄 필요가 없다.
-               return _currentNode;
-            }
-            else // input height가 더 높다는 의미이므로 새로운 높이로 반환한다.
-            {
-               _currentNode->height = input_building->height;  //현재 노드의 높이를 인풋빌딩의 높이로 바꾸어 준다.
-               return _currentNode;
-            }
-         }
-         else { // 같은 경우 높이도 같고 위치도 같은 경우이므로 새로 고쳐줄 것이 없다.
-            return _currentNode;
-         }
-      }
-   }
 
-   Node* connect(Node* left, Node* right) {
-      left->next = right;
-   }
-   void merge(const skyline &a){
+	void merge(const skyline &input_skyline){
+		if (input_skyline.head == NULL) // merge할 skyline의 head가 비어있으면 아무것도 할 수 있는게 없다.
+		{
+			cout << "This is empty skyline" << endl;
+			return;
+		}
+		else if (head == NULL) { // 처음 merge 하는 경우 A는 비어있는 리스트이므로
+			cout << "연결하겠습니다" << endl;
+			head = input_skyline.head;
 
-   }
-   void print() {
-      while (head != NULL) {
-         cout << "( " << head->location <<", "<<head->height << " ) ->";
-         head = head->next;
-      }
-   }
-   void clear() {
-      while (head != NULL) {
-         Node* tmp = head;
-         head = head->next;
-         delete tmp;
-      }
-      cout << "* Delete all nodes" << endl;
-   }
+		}
+		else  // 이미 merge 되어 있는 list에 새로 merge를 해야 하므로
+		{
+			Node* temp = head;
+			while (temp->next != NULL)
+			{
+				temp = temp->next;
+			}temp->next = input_skyline.head; // 리스트의 마지막 노드에 새로 merge할 리스트의 헤드를 연결한다. --> sorting이 필요한 상태
+		}
+
+		sorting(&head); // sorting 함수를 정의해서 sorting 해준다.
+	}
+	void sorting(Node** _currentNode) {
+		Node* head = *_currentNode;
+		Node* a;
+		Node* b;
+
+		/* Base case -- length 0 or 1 */
+		if ((head == NULL) || (head->next == NULL)) {
+			return;
+		}
+
+		/* Split head into 'a' and 'b' sublists */
+		FrontBackSplit(head, &a, &b);
+
+		/* Recursively sort the sublists */
+		sorting(&a);
+		sorting(&b);
+
+		/* answer = merge the two sorted lists together */
+		*_currentNode = SortedMerge(a, b);
+	}
+	void FrontBackSplit(Node* source,
+		Node** frontRef, Node** backRef)
+	{
+		Node* fast;
+		Node* slow;
+		slow = source;
+		fast = source->next;
+
+		/* Advance 'fast' two nodes, and advance 'slow' one node */
+		while (fast != NULL) {
+			fast = fast->next;
+			if (fast != NULL) {
+				slow = slow->next;
+				fast = fast->next;
+			}
+		}
+
+		/* 'slow' is before the midpoint in the list, so split it in two
+		at that point. */
+		*frontRef = source;
+		*backRef = slow->next;
+		slow->next = NULL;
+	}
+
+	Node* SortedMerge(Node* a, Node* b)
+	{
+		Node* result = NULL;
+		/* Base cases */
+		if (a == NULL)
+			return (b);
+		else if (b == NULL)
+			return (a);
+
+		/* Pick either a or b, and recur */
+		if (a->location <= b->location) {
+			result = a;
+			result->next = SortedMerge(a->next, b);
+		}
+		else {
+			result = b;
+			result->next = SortedMerge(a, b->next);
+		}
+		return (result);
+	}
+
+	void print() {
+
+		if (head != NULL) {
+			Node* temp = head;
+
+			while (temp != NULL) {
+
+				cout << "( " << temp->location << ", " << temp->height << " ) ->";
+				temp = temp->next;
+			}
+			cout << "NULL" << endl;
+		}
+		else {
+			cout << "출력할 리스트가 없습니다!" << endl;
+		}
+
+	}
+
+	void flex(Node* _current) {
+		Node* deleteNode;
+		if (_current != NULL) {
+			if (_current->location = _current->next->location) {
+				if (_current->height < _current->next->height) {
+					_current->height = _current->next->height;
+					deleteNode = _current->next;
+					_current->next = _current->next->next;
+					delete deleteNode;
+				}
+				else { // 같거나 큰 경우
+					deleteNode = _current->next;
+					_current->next = _current->next->next;
+					delete deleteNode;
+				}
+			}
+			else { // 위치는 다르지만 높이가 다를 경우
+				if (_current->height > _current->next->height) {
+
+				}
+				else if (_current->height > _current->next->height) {
+
+				}
+				else {
+
+				}
+			}
+		}
+	}
+
+	void clear() {
+		while (head != NULL) {
+			Node* tmp = head;
+			head = head->next;
+			delete tmp;
+		}
+		cout << "* Delete all nodes" << endl;
+	}
 
 };
-int main()
-{
-   skyline A(1,3,3);
-   A.print();
-   skyline A(6, 4, 20);
-   A.print();
-   return 0;
-}
-/*
+
 int main() {
 
-   skyline A;
+	skyline A;
 
-   A.merge(skyline(1, 3, 3));
-   A.print();
-   A.merge(skyline(6, 4, 20));
-   A.print();
-   A.merge(skyline(20, 10, 17));
-   A.print();
-   A.merge(skyline(3, 9, 19));
-   A.print();
-   A.merge(skyline(16, 20, 14));
-   A.print();
-   A.merge(skyline(1, 17, 19));
-   A.print();
-   A.merge(skyline(14, 16, 15));
-   A.print();
+	A.merge(skyline(1, 3, 3));
+	A.print();
+	A.merge(skyline(6, 4, 20));
+	A.print();
+	A.merge(skyline(20, 10, 17));
+	A.print();
+	A.merge(skyline(3, 9, 19));
+	A.print();
+	A.merge(skyline(16, 20, 14));
+	A.print();
+	A.merge(skyline(1, 17, 19));
+	A.print();
+	A.merge(skyline(14, 16, 15));
+	A.print();
 
-   return 0;
+	return 0;
 }
-*/
