76c10abe053c13303dfc7390b22745329a001c01
minsoo kim <zlemzlem5656@naver.com>
2019-12-18 00:16:15 +0900

    it is experience for sorting function in C++
    I saw and tried this code on this blog.
    http://gmlwjd9405.github.io/

--- Sorting/merge.cpp
+++ Sorting/merge.cpp
@@ -1,6 +1,7 @@
-#include <stdio.h>
-#define MAX_SIZE 8 
-int sorted[MAX_SIZE] ;
+#include <iostream>
+using namespace std;
+
+int sorted[8] ;
 // 추가적인 공간이 필요 
 // i: 정렬된 왼쪽 리스트에 대한 인덱스
 // j: 정렬된 오른쪽 리스트에 대한 인덱스 
@@ -10,7 +11,10 @@ int sorted[MAX_SIZE] ;
 /* (실제로 숫자들이 정렬되는 과정) */ 
 void merge(int list[], int left, int mid, int right)
 { 
-    int i, j, k, l; i = left; j = mid+1; k = left; 
+    int i, j, k, l; 
+    i = left; 
+    j = mid+1;
+    k = left; 
     /* 분할 정렬된 list의 합병 */ 
     while(i<=mid && j<=right)
     { 
@@ -43,22 +47,26 @@ void merge_sort(int list[], int left, int right)
     {
         mid = (left+right)/2 ;
         // 중간 위치를 계산하여 리스트를 균등 분할 -분할(Divide)
+        
         merge_sort(list, left, mid); // 앞쪽 부분 리스트 정렬 -정복(Conquer)
         merge_sort(list, mid+1, right); // 뒤쪽 부분 리스트 정렬 -정복(Conquer)
         merge(list, left, mid, right); // 정렬된 2개의 부분 배열을 합병하는 과정 -결합(Combine) 
     } 
 } 
 
+void print(int* printarray ,int n){
+    for(int i=0; i<n; i++){
+        cout<< printarray[i] <<", " ;
+    }
+    cout << "end" << endl;
+}
 void main()
 { 
     int i; 
-    int n = MAX_SIZE; 
+    int n = 8; 
     int list[8] = {21, 10, 12, 20, 25, 13, 15, 22}; 
     // 합병 정렬 수행(left: 배열의 시작 = 0, right: 배열의 끝 = 7)
      merge_sort(list, 0, n-1); 
     // 정렬 결과 출력 
-    for(i=0; i<n; i++)
-    {
-     printf("%d\n", list[i]); 
-    } 
+    print(list, 8);
 }
