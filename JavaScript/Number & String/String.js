c9c9123c23b9e8d8a7f202b6ae2e8b144a00b5c3
minsoo kim <zlemzlem5656@naver.com>
2019-12-23 16:34:49 +0900

    자바스크립트 공부를 시작했습니다
    01. Number & String

---
+++ JavaScript/Number & String/String.js
@@ -0,0 +1,15 @@
+document.write("this is string <br> " );
+
+document.write("this is string <br> this is another string");
+
+alert("this is string \nthis is another string") ;
+
+// string.charCodeAt(index)
+// index에 해당하는 문자의 unicode 값을 리턴
+
+var string1 = "hello world"
+alert(document.write(string1.charCodeAt(0)));  // H의 unicode 값
+
+// string1.concat(string2,string3....)
+// string1에 인자로 받은 스트링 값들을 합쳐준다. (순서대로)
+
