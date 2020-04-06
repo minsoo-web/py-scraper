d75cc22f899775e8097bf9feba4a56b932a0f014
Minsoo <zlemzlem5656@naver.com>
2020-04-07 00:59:33 +0900

    Add : 크레인 문제 해결 버전

---
+++ Programmers/kakao/크레인 해결.js
@@ -0,0 +1,28 @@
+function solution(board, moves) {
+  let answer = 0;
+
+  let stack = [];
+
+  for (let x of moves) {
+    x = x - 1;
+    for (let i = 0; i < board.length; i++) {
+      if (board[i][x] !== 0) {
+        stack.push(board[i][x]);
+        board[i][x] = 0;
+        break;
+      }
+    }
+
+    while (stack.length > 1) {
+      let temp = stack.pop();
+      if (temp === stack.slice(-1)[0]) {
+        stack.pop();
+        answer += 2;
+      } else {
+        stack.push(temp);
+        break;
+      }
+    }
+  }
+  return answer;
+}
