06fd3fb5337057b27067c81b6ba84511b0da2016
minsoo-web <zlemzlem5656@naver.com>
2020-12-31 13:21:54 +0900

    [수정] argparse 을 사용해서 하나의 app.py로 통합

--- app.py
+++ app.py
@@ -1,27 +1,72 @@
 import argparse
+import sys
 
-from naver_shop import naver_bs4
+from naver_shop import naver_review  # csv
+from naver_related import naver_related  # 연관 검색어
+from naver_search import naver_search
 from save import save_to_csv
+
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description='argparser!')
-    parser.add_argument('--url', '-u', required=True, help='Target URL')
+    parser = argparse.ArgumentParser(description='naver shop crawler')
+    parser.add_argument('target', help='review(csv)) / related(re) / keyword(key)')
+    parser.add_argument('--url', '-u', required=False, help='Target URL')
     parser.add_argument('--limit', '-l', required=False,
                         help='last page for scraping', default="end")
+    parser.add_argument('--item', '-i', required=False, help='Target item')
     args = parser.parse_args()
 
-    NAVER_SHOP_URL = args.url
+    # 쇼핑 리뷰 to csv
+    if args.target in ['review', 'csv']:
+        if args.url is None:
+            print("error: the following arguments are required: --url/-u. make sure url")
+            sys.exit(1)
 
-    naver_shop = naver_bs4(NAVER_SHOP_URL)
+        NAVER_SHOP_URL = args.url
+        naver = naver_review(NAVER_SHOP_URL)
 
-    if args.limit == "end":
-        # limit을 인자로 주지 않으면 default로 모든 페이지
-        last_page = naver_shop.get_last_page()
+        if args.limit == "end":
+            # limit을 인자로 주지 않으면 default로 모든 페이지
+            last_page = naver.get_last_page()
+        else:
+            # limit을 받았을 경우
+            last_page = int(args.limit)
+
+        reviews = naver.get_reviews(last_page)
+
+        save_to_csv(reviews)
+
+    # 연관 검색어 및 전체 아이템 갯수
+    elif args.target in ['related', 're']:
+        if args.item is None:
+            print("error: the following arguments are required: --item/-i. make sure item")
+            sys.exit(1)
+        naver = naver_related(args.item)
+        number_of_items = naver.get_all_items()
+        arr_of_related = naver.get_related_items()
+        print(f"검색된 아이템 갯수: {number_of_items}")
+        print(f"연관 검색어: {arr_of_related}")
+
+    # 키워드 분석 -> 카테고리 순서, 섹션 순서
+    elif args.target in ['keyword', 'key']:
+        if args.item is None:
+            print("error: the following arguments are required: --item/-i. make sure item")
+            sys.exit(1)
+        naver = naver_search(args.item)
+        desktop_category_list = naver.get_search_category('desktop')
+        mobile_category_list = naver.get_search_category('mobile')
+        print("<카테고리 키워드 리스트>")
+        print(f"Desktop: {desktop_category_list}")
+        print(f"Mobile: {mobile_category_list}")
+
+        desktop_section_list = naver.get_section_list('desktop')
+        mobile_section_list = naver.get_section_list('mobile')
+        print("<섹션 키워드 리스트>")
+        print(f"Desktop: {desktop_section_list}")
+        print(f"Mobile: {mobile_section_list}")
+
+    # csv / related / keyword 가 아닌 경우 exit
     else:
-        # limit을 받았을 경우
-        last_page = int(args.limit)
-
-    reviews = naver_shop.get_reviews(last_page)
-
-    save_to_csv(reviews)
+        print("usage: app.py [csv / related / keyword]")
+        sys.exit(1)
--- naver_search.py
+++ naver_search.py
@@ -1,137 +1,117 @@
-import argparse
 import requests
 
 from bs4 import BeautifulSoup
 
 
-def return_soup(url):
-    """soup 반환하는 함수
+class naver_search:
+    def __init__(self, item):
+        BASE_URL = f"search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query={item}"
 
-    Args:
-        url (str): 검색될 페이지 url
+        self.__DESKTOP_URL = f"https://{BASE_URL}"
+        self.__MOBILE_URL = f"https://m.{BASE_URL}"
 
-    Returns:
-        bs4.BeautifulSoup
-    """
+    def __return_soup(self, soup_type):
+        """soup 반환하는 메서드
 
-    page = requests.get(url)
-    soup = BeautifulSoup(page.text, 'html.parser')
-    return soup
+        Args:
+            url (str): 검색될 페이지 url
 
+        Returns:
+            bs4.BeautifulSoup
+        """
 
-def remove_useless_element(find_soup):
-    """soup 내에서 불필요한 요소들을 삭제하는 함수
+        if soup_type == 'desktop':
+            page = requests.get(self.__DESKTOP_URL)
+        else:
+            page = requests.get(self.__MOBILE_URL)
 
-    Args:
-        find_soup (bs4.BeautifulSoup): 제거가 필요한 부모 soup
-    """
-    div = find_soup.find('div')
-    i = find_soup.find('i')
-    a = find_soup.find('a')
+        soup = BeautifulSoup(page.text, 'html.parser')
+        return soup
 
-    if div is not None:
-        div.decompose()
-    if i is not None:
-        i.decompose()
-    if a is not None:
-        a.decompose()
+    def __remove_useless_element(self, find_soup):
+        """soup 내에서 불필요한 요소들을 삭제하는 메서드
 
+        Args:
+            find_soup (bs4.BeautifulSoup): 제거가 필요한 부모 soup
+        """
+        div = find_soup.find('div')
+        i = find_soup.find('i')
+        a = find_soup.find('a')
 
-def get_search_category(url, soup_type):
-    """검색이 되면 네이버 검색 페이지의 탭 순서를 list 로 반환
+        if div is not None:
+            div.decompose()
+        if i is not None:
+            i.decompose()
+        if a is not None:
+            a.decompose()
 
-    Args:
-        url (str): 검색될 페이지 url
-        soup_type (str): mobile인지 desktop인지
+    def get_search_category(self, soup_type):
+        """검색이 되면 네이버 검색 페이지의 탭 순서를 list 로 반환
 
-    Returns:
-        list: 검색 페이지의 탭을 순서대로 담은 list
-    """
+        Args:
+            url (str): 검색될 페이지 url
+            soup_type (str): mobile인지 desktop인지
 
-    soup = return_soup(url)
-    result = []
+        Returns:
+            list: 검색 페이지의 탭을 순서대로 담은 list
+        """
 
-    desktop_selector = '#lnb [role="tablist"] > li'  # 웹 뷰 선택자
-    mobile_selector = 'header [role="tablist"] > li span'  # 모바일 뷰 선택자
+        soup = self.__return_soup(soup_type)
+        result = []
 
-    if soup_type == 'desktop':
-        selector = desktop_selector
-    else:
-        selector = mobile_selector
+        desktop_selector = '#lnb [role="tablist"] > li'  # 웹 뷰 선택자
+        mobile_selector = 'header [role="tablist"] > li span'  # 모바일 뷰 선택자
 
-    _result = soup.select(selector)
+        if soup_type == 'desktop':
+            selector = desktop_selector
+        else:
+            selector = mobile_selector
 
-    # 텍스트만 뽑는 작업
-    for tabitem in _result:
-        if soup_type != 'desktop':
-            remove_useless_element(tabitem)  # 모바일의 경우 tablist에도 i 가 있음
-        result.append(tabitem.text)
-
-    return result
-
-
-def get_section_list(url, soup_type):
-    """네이버 검색 페이지에서 섹션별 순서를 담아 list로 반환
-
-    Args:
-        url (str): 검색될 페이지 url
-        soup_type (str): mobile인지 desktop인지
-
-    Returns:
-        list: 섹션 타이틀을 순서대로 담은 list
-    """
-
-    soup = return_soup(url)
-    result = []
-    # desktop_selector = '#main_pack > section .api_title'
-    # 광고, 파워링크, 화면에 나오지 않는 추천 트렌드 섹션은 제외
-    mobile_selector = '#container section:not(.nx_pla):not(.sp_power) .api_subject_bx:not(._au_recommend_trend) h2'
-
-    if soup_type == 'desktop':
-        # selector = desktop_selector
-        _result = soup.findAll('h2', {'class': 'api_title'})
-    else:
-        selector = mobile_selector
         _result = soup.select(selector)
 
-    for section_item in _result:
-        title = ""
-        # h2 태그 안의 불필요한 요소들을 모두 삭제
-        remove_useless_element(section_item)
+        # 텍스트만 뽑는 작업
+        for tabitem in _result:
+            if soup_type != 'desktop':
+                self.__remove_useless_element(tabitem)  # 모바일의 경우 tablist에도 i 가 있음
+            result.append(tabitem.text)
 
-        title = section_item.text
-        # 네이버 쇼핑 라이브의 경우 strong 태그 안의 내용이 타이틀이다
-        strong = section_item.find('strong')
-        if strong is not None:
-            title = strong.text
+        return result
 
-        result.append(title.strip())
+    def get_section_list(self, soup_type):
+        """네이버 검색 페이지에서 섹션별 순서를 담아 list로 반환
 
-    return result
+        Args:
+            url (str): 검색될 페이지 url
+            soup_type (str): mobile인지 desktop인지
 
+        Returns:
+            list: 섹션 타이틀을 순서대로 담은 list
+        """
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='argparser!')
-    parser.add_argument('--item', '-i', required=True, help='Target item')
-    args = parser.parse_args()
+        soup = self.__return_soup(soup_type)
+        result = []
+        # desktop_selector = '#main_pack > section .api_title'
+        # 광고, 파워링크, 화면에 나오지 않는 추천 트렌드 섹션은 제외
+        mobile_selector = '#container section:not(.nx_pla):not(.sp_power) .api_subject_bx:not(._au_recommend_trend) h2'
 
-    BASE_URL = f"search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query={args.item}"
+        if soup_type == 'desktop':
+            # selector = desktop_selector
+            _result = soup.findAll('h2', {'class': 'api_title'})
+        else:
+            selector = mobile_selector
+            _result = soup.select(selector)
 
-    DESKTOP_URL = f"https://{BASE_URL}"
-    MOBILE_URL = f"https://m.{BASE_URL}"
+        for section_item in _result:
+            title = ""
+            # h2 태그 안의 불필요한 요소들을 모두 삭제
+            self.__remove_useless_element(section_item)
 
-    # 데스크톱 기준 카테고리 순서
-    de_arr_tablist = get_search_category(DESKTOP_URL, 'desktop')
-    # 모바일 기준 카테고리 순서
-    m_arr_tablist = get_search_category(MOBILE_URL, 'mobile')
+            title = section_item.text
+            # 네이버 쇼핑 라이브의 경우 strong 태그 안의 내용이 타이틀이다
+            strong = section_item.find('strong')
+            if strong is not None:
+                title = strong.text
 
-    # 데스크톱 기준 섹션 순서
-    de_arr_sectionlist = get_section_list(DESKTOP_URL, 'desktop')
-    # 모바일 기준 섹션 순서
-    m_arr_sectionlist = get_section_list(MOBILE_URL, 'mobile')
+            result.append(title.strip())
 
-    print(de_arr_tablist)
-    print(m_arr_tablist)
-
-    print(de_arr_sectionlist)
-    print(m_arr_sectionlist)
+        return result
